a29e052ed45b15702b4b3a608517728a485d376a
paras151 <parasmadan15@gmail.com>
2023-11-12 22:26:24 +0530

    used encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CenteredH1Component } from './centered-h1/centered-h1.component';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, CenteredH1Component],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'angular-rev-aniruddha';
